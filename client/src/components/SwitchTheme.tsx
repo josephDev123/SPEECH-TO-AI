a0600dfb92b4f2d1a7589a114a972225cde0af1a
josephDev123 <josephuzuegbu55@gmail.com>
2025-06-18 12:43:37 +0100

    fix mode

--- client/src/components/SwitchTheme.tsx
+++ client/src/components/SwitchTheme.tsx
@@ -1,15 +1,23 @@
 import { ThemeContext } from "@/context/Theme";
 import { Sun, Moon } from "lucide-react";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 export default function SwitchTheme() {
   const { theme, setTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    const ColorTheme =
+      localStorage.getItem("theme") || (theme as "light" | "dark");
+    document.documentElement.classList.add(ColorTheme);
+    setTheme(ColorTheme as "light" | "dark");
+  }, [theme]);
 
   return (
     <button
       type="button"
       onClick={() => {
         const newTheme = theme === "light" ? "dark" : "light";
+        localStorage.setItem("theme", newTheme);
         setTheme(newTheme);
 
         if (newTheme === "dark") {
